4ccc1ea08bf91a08fe7000d2ec8edd48358f01fd
Dan Parker <dmccracken123@gmail.com>
2020-02-19 11:44:40 -0700

    Update regression.py

--- smm2sim/regression.py
+++ smm2sim/regression.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 
 class Regression(object):
-    def __init__(self, to=None, weight=None, n_games=33):
+    def __init__(self, to=None, weight=None, n_games=16):
         self.regression_values = to
         self.regression_weight = weight
         self.num_games = n_games
@@ -15,7 +15,7 @@ class Regression(object):
             reg_values = [self.regression_values] * df.shape[0]
             mydf = df
         else:
-            merged = pd.merge(self.regression_values, df, on='Player')
+            merged = df.merge(self.regression_values, on='Team')
             reg_values = merged['Baseline'].values
             mydf = merged
         if self.regression_weight is not None:
@@ -23,12 +23,9 @@ class Regression(object):
             played_weight = (1 - self.regression_weight) * df.shape[0]
             weight_sum = 1
         else:
-            grouped = mydf.groupby('Player').agg({pwrcol:'count'}).reset_index()
-            mydf = pd.merge(mydf, grouped.rename(columns={pwrcol:'Games Played'}), on='Player')
             reg_weight = np.maximum(self.num_games - mydf['Games Played'].values, [0] * mydf.shape[0])
             played_weight = mydf['Games Played'].values
             weight_sum = reg_weight + played_weight
-            mydf = mydf[[x for x in list(mydf) if x != 'Games Played']]
         played_weighted = mydf[pwrcol].values * played_weight
         regressed_weighted =  reg_values * reg_weight
         return (played_weighted + regressed_weighted) / weight_sum
