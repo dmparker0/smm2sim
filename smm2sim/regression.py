2d0a4fffa75a8dc7d9034f0bfe4be9cf17714705
Dan Parker <dmccracken123@gmail.com>
2020-02-19 13:05:12 -0700

    Update regression.py

--- smm2sim/regression.py
+++ smm2sim/regression.py
@@ -12,15 +12,15 @@ class Regression(object):
         if self.regression_values is None:
             self.regression_values = np.mean(df[pwrcol].values)
         if type(self.regression_values) is not pd.DataFrame:
-            reg_values = [self.regression_values] * df.shape[0]
+            reg_values = np.array([self.regression_values] * df.shape[0])
             mydf = df
         else:
             merged = df.merge(self.regression_values, on='Player')
             reg_values = merged['Baseline'].values
             mydf = merged
         if self.regression_weight is not None:
-            reg_weight = self.regression_weight * df.shape[0]
-            played_weight = (1 - self.regression_weight) * df.shape[0]
+            reg_weight = [self.regression_weight] * df.shape[0]
+            played_weight = [(1 - self.regression_weight)] * df.shape[0]
             weight_sum = 1
         else:
             reg_weight = np.maximum(self.num_games - mydf['Games Played'].values, [0] * mydf.shape[0])
